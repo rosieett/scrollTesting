75b4613230511499fb18b9228683145a9ad2fb7f
rosieett <58713682+rosieett@users.noreply.github.com>
2022-01-24 14:07:20 -0500

    background image update

--- app.js
+++ app.js
@@ -1,46 +1,40 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    let textBoxes = document.querySelectorAll('.box');
-    let textBoxArray = Array.from(textBoxes);
-  
-    let images = document.querySelectorAll('.imgFade');
-    let imagesArray = Array.from(images);
-  
-    window.addEventListener('scroll', (event) => {
+  let textBoxes = document.querySelectorAll('.box');
+  let textBoxArray = Array.from(textBoxes);
 
-  
-      let viewportHeight = window.innerHeight; //the window height is what you see
-      let scrollCenter = viewportHeight / 2; //how many pixels down from the top of the page we've scrolled
-  
-      let textBoxIndex = textBoxArray.findIndex(box => {
-        let rect = box.getBoundingClientRect();
-  
-//         console.log(rect.top, 'textTop');
-//         console.log(rect.bottom, 'textBottom');
-  
-        if (rect.top <= scrollCenter && rect.bottom >= 0) {
-          return true;
-        }
-  
-      });
-  
-      if (textBoxIndex !== -1) {
-  
-//         console.log(textBoxIndex);
-  
-        let activeImage = imagesArray[textBoxIndex];
-  
-//         console.log(activeImage);
-  
-        imagesArray.forEach(img => img.classList.remove('active'));
-        activeImage.classList.add('active');
+  let images = document.querySelectorAll('.imgFade');
+  let imagesArray = Array.from(images);
+
+  window.addEventListener('scroll', (event) => {
+
+    let viewportHeight = window.innerHeight; //the window height is what you see
+    let scrollCenter = viewportHeight / 2; //how many pixels down from the top of the page we've scrolled
+
+    let textBoxIndex = textBoxArray.findIndex(box => {
+      let rect = box.getBoundingClientRect();
+
+
+      if (rect.top <= scrollCenter && rect.bottom >= 0) {
+        return true;
       }
 
-  
-    })
-  
-  
-  
-  });
-  
+    });
+    
+    if (textBoxIndex !== -1) {
+      let activeImage = imagesArray[textBoxIndex];
+      imagesArray.forEach(img => img.classList.remove('active'));
+      activeImage.classList.add('active');
+      
+      // Remove the initial class if it exists
+      if (activeImage.classList.contains('active-initial')) {
+          activeImage.classList.remove('active-initial');
+      }
+    }
+
+  })
+
+
+
+});
